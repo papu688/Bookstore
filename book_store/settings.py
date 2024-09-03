4af03dcf354395a5059176f320adc0eaa205f0ec
papu_68 <giorgobianipaul@gmail.com>
2024-09-03 19:12:20 +0200

    Switch database backend to PosgreSQL - updated `DATABASES`configuration in `settings.py`to use PosgreSQL. - Configured database name, user, password, host, and port.

--- book_store/settings.py
+++ book_store/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'book_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Bookstore',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
